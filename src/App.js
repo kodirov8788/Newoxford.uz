882027d849add0b2c8e53533188e91f733bae5b7
IsoxonMirzaev <isoxonmirzayev551@gmail.com>
2024-01-19 18:28:29 +0500

    problem solved

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react'
 import Navbar from './components/navbar/navbar'
 
 import Footer from './footer/Footer';
-import Hidden_swiper from "./components/Hidden_swiper";
 import Accordion from "./Accordion/Accordion"
 import Manzil from './components/Manzil/Manzil';
 
@@ -17,8 +16,6 @@ function App() {
       <Navbar open={open} setOpen={setOpen} />
       <Main />
       <Manzil/>
-      <Hidden_swiper />
-      <I_box />
       <Accordion />
       <Footer />
     </div>
