ee6c00a46af8af9488cc72885ec99873fe16d96e
IsoxonMirzaev <isoxonmirzayev551@gmail.com>
2024-02-04 09:01:07 +0500

    problem fixed

--- src/components/ielts/Ielts.js
+++ src/components/ielts/Ielts.js
@@ -58,55 +58,13 @@ function Ielts() {
                   IELTS Pro
                 </h1>
               </div>
-
-              <div className="w-full h-[90px] flex items-center justify-center group duration-1000 mt-10 hover:bg-blue-600 shadow-lg border shadow-indigo-500/100 rounded-2xl">
-                <h1 className="md:text-[40px] text-[32px] text-blue-600 duration-1000 group-hover:text-white hover:text-white">
-                  Intensive
-                </h1>
-              </div>
-
-              <div className="w-full h-[90px] flex items-center group justify-center duration-1000 hover:bg-blue-600 mt-10 shadow-lg border shadow-indigo-500/100 rounded-2xl">
-                <h1 className="md:text-[40px] text-[32px] group-hover:text-white duration-1000 text-blue-600 hover:text-white">
-                  CEFR
-                </h1>
-              </div>
             </div>
-          </div>
-
-          <div className="w-full h-[90px] flex items-center justify-center group duration-1000 mt-10 hover:bg-blue-600 shadow-lg border shadow-indigo-500/100 rounded-2xl">
-            <h1 className="md:text-[40px] text-[32px] text-blue-600 duration-1000 group-hover:text-white hover:text-white">
-              Pre-Intermediate
-            </h1>
-          </div>
-
-          <div className="w-full h-[90px] flex items-center group justify-center duration-1000 hover:bg-blue-600 mt-10 shadow-lg border shadow-indigo-500/100 rounded-2xl">
-            <h1 className="md:text-[40px] text-[32px] group-hover:text-white duration-1000 text-blue-600 hover:text-white">
-              Intermediate
-            </h1>
-          </div>
-        </div>
-      </div>
-      <div className="md:w-[50%]  w-full md:h-full h-[700px] flex items-center justify-center pt-5 pb-5 ">
-        <div className="w-[90%] h-[59%] bg-white shadow-lg border shadow-indigo-500/100 rounded-2xl pt-5 pl-8 pr-8">
-          <div className="w-full h-[70px] flex items-center justify-center ">
-            <h1 className="md:text-[40px] text-[35px] text-blue-600 ">IELTS</h1>
-          </div>
-          <div className="w-full h-[90px] flex items-center justify-center duration-1000 group hover:bg-blue-600 mt-5 shadow-lg border shadow-indigo-500/100 rounded-2xl">
-            <h1 className="md:text-[40px] text-[32px] text-blue-600 duration-1000 group-hover:text-white">
-              IELTS Foundation
-            </h1>
-          </div>
-
-          <div className="w-full h-[90px] flex items-center justify-center group duration-1000 mt-10 hover:bg-blue-600 shadow-lg border shadow-indigo-500/100 rounded-2xl">
-            <h1 className="md:text-[40px] text-[32px] text-blue-600 group-hover:text-white duration-1000 hover:text-white">
-              IELTS Pro
-            </h1>
           </div>
         </div>
       </div>
       <div className="w-[47.5%] mx-auto my-5 h-full bg-white shadow-lg border shadow-indigo-500/100 rounded-2xl pt-5 pl-8 pr-8">
         <div className="w-full h-[70px] flex items-center justify-center ">
-          <h1 className="md:text-[40px] text-[35px] text-blue-600 ">
+          <h1 className="md:text-[40px] text-[35px] text-blue-600 font-bold">
             Boshqa kurslar
           </h1>
         </div>
